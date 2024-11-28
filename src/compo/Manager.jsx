8bff09932597777b6b6c6156ec4754c81c3bbad4
Mayank <gautambmayank@gmail.com>
2024-11-28 14:07:19 +0530

    change1

--- src/compo/Manager.jsx
+++ src/compo/Manager.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const Manager = () => {
   const deletebutton = "src/assets/deletebutton.png";
